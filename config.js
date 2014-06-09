64611339a06922c0b3816f1b818886d44763972f
gtracy <gtracy@gmail.com>
2014-06-08 23:10:01 -0500

    added better comments

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  // Stuart server parameters
+  // Stuart server parameters required for Twilio integration
   "host"   : "http://FIXME",
   "port"   : "8087",
 
@@ -9,6 +9,8 @@ module.exports = {
   "emoji"  : ":construction_worker:",
 
   // The Slack host
+  //
+  // Careful! Don't include the token with your slack_host value
   "slack_host"  : "https://FIXME.slack.com/services/hooks/incoming-webhook",
   "slack_token" : "FIXME"
 
