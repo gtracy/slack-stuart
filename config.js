8ef32009d053a8a8ec7fd6edebf70cf9a89ea53a
Charlie Lor <lorcharlie@gmail.com>
2016-08-11 12:05:42 -0500

    More formatting

--- config.js
+++ config.js
@@ -15,4 +15,5 @@ module.exports = {
   // Careful! Don't include the token with your slack_host value
   "slack_host"  : "https://hooks.slack.com/services/FIXME/FIXME/FIXME",
   "slack_token" : "FIXME"
+
 };
