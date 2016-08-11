4e529564ffb5f658a1da5b0d940c902180391667
Charlie Lor <lorcharlie@gmail.com>
2016-08-11 11:52:56 -0500

    Readded the keys back to config.js

--- config.js
+++ config.js
@@ -13,5 +13,6 @@ module.exports = {
   // The Slack host
   //
   // Careful! Don't include the token with your slack_host value
-
+	"slack_host"  : "https://hooks.slack.com/services/FIXME/FIXME/FIXME",		
+	"slack_token" : "FIXME"
 };
