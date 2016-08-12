ef5898ff0cf6f48eafa285c126701706153c3c5f
Charlie Lor <lorcharlie@gmail.com>
2016-08-12 16:48:05 -0500

    Using Promise object instead of callback and added another simple test

--- test/plugins/slash/thesaurus.js
+++ test/plugins/slash/thesaurus.js
@@ -1,7 +1,7 @@
-var rewire = require('rewire');
 var assert = require('assert');
 var chai = require('chai');
-var thesaurus = rewire('../../../lib/plugins/slash/thesaurus');
+// var chaiAsPromise = require('chai-as-promise');
+var thesaurus = require('../../../lib/plugins/slash/thesaurus');
 
 describe("Thesaurus", function() {
 	var mockRequest = { "user_name": "foobar" };
@@ -22,31 +22,34 @@ describe("Thesaurus", function() {
 		it("Should display description and usage", function() {
 			thesaurus.help(mockRequest, mockStuart);
 			
-			chai.expect(mockStuart.postContent).to.contain("Find synonyms and antonyms of a given word.\n\n Usage: */stuart thesaurus [word]*");
+			chai.expect(mockStuart.postContent).to.contain("Find synonyms, antonyms, similar and related words of a given word.\n\nUsage: */stuart thesaurus [word]*");
 			chai.expect(mockStuart.postCalled).to.be.true;
 		});
 	});
 	
 	describe("#run()", function() {
+		it ("Should display error if configuration is set up improperly", function() {
+			thesaurus.run(mockRequest, ['greater'], mockStuart, { "config": {}});
+			
+			chai.expect(mockStuart.postContent).to.contain("Incorrect configuration -- Please fix!");
+			chai.expect(mockStuart.postCalled).to.be.true;
+		});
+		
 		it("Should display error and usage if the number args is greater than one", function() {
 			thesaurus.run(mockRequest, ['greater', 'one'], mockStuart, { "config": { "key": "apikey", "version": 2 }});
 			
-			chai.expect(mockStuart.postContent).to.contain("Incorrect number of arguments.\n\n Usage: */stuart thesaurus [word]*");
+			chai.expect(mockStuart.postContent).to.contain("Incorrect number of arguments.\n\nUsage: */stuart thesaurus [word]*");
 			chai.expect(mockStuart.postCalled).to.be.true;
 		});
 		
 		it("Should display error and usage if the number args is less than one", function() {
 			thesaurus.run(mockRequest, [], mockStuart, { "config": { "key": "apikey", "version": 2 }});
 			
-			chai.expect(mockStuart.postContent).to.contain("Incorrect number of arguments.\n\n Usage: */stuart thesaurus [word]*");
+			chai.expect(mockStuart.postContent).to.contain("Incorrect number of arguments.\n\nUsage: */stuart thesaurus [word]*");
 			chai.expect(mockStuart.postCalled).to.be.true;
 		});
 		
-		it("Should output correctly using the word 'user'", function() {
-			var result = {"noun":{"syn":["somebody","someone","soul"]}}
-			
-			thesaurus.__set__("getWords", {"noun":{"syn":["somebody","someone","soul"]}});
-			
+		it("Should output correctly using the word 'user'", function() {			
 			var args = ['user'];
 			thesaurus.run(mockRequest, args, mockStuart, { "config": { "key": "apikey", "version": 2 }});
 			
